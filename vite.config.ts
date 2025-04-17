2137bdd7f7663f37b5cc56429f2e9208c9d4fe83
Nikita <nikitalomieiko@gmail.com>
2025-04-17 16:28:45 +0300

    fix auto path

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      app: "/src/app",
+      pages: "/src/pages",
+      widgets: "/src/widgets",
+      features: "/src/features",
+      entities: "/src/entities",
+      shared: "/src/shared",
+    },
+  },
 });
